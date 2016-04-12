04d370c93b62b55ab449bcb83890a9e1007f23a8
David Gonzalez <David@Davids-MacBook-Pro.local>
2016-04-12 16:13:53 -0400

    Add logic to connect to Teradata based on platform - Add connection thorugh JDBC when not a mac platform

--- atp_classes/TeradataDB.py
+++ atp_classes/TeradataDB.py
@@ -1,5 +1,4 @@
-import teradata
-import atp_classes, re
+import atp_classes, re, platform, os
 
 
 class TeradataDB:
@@ -12,30 +11,76 @@ class TeradataDB:
 
     def execute_query(self, query_string):
         result_rows = []
-        udaExec = teradata.UdaExec(appName="DataFetcher", version="1.0", logConsole=False)
 
-        with udaExec.connect(method="odbc", system=self.host, username=self.username, password=self.password)as conn:
-            with conn.cursor() as cur:
-                try:
-                    print "executing query"
+        if platform.mac_ver()[0] != '':
+            import teradata
 
-                    # Execute query
-                    cur.execute(query_string)
+            udaExec = teradata.UdaExec(appName="DataFetcher", version="1.0", logConsole=False)
 
-                    print "done executing query"
+            with udaExec.connect(method="odbc", system=self.host, username=self.username, password=self.password)as conn:
+                with conn.cursor() as cur:
+                    try:
+                        print "executing query"
 
-                    # Get column names
-                    columns = cur.description
+                        # Execute query
+                        cur.execute(query_string)
 
-                    # Fetch table results
-                    for row in cur:
-                        result_obj = {}
-                        for index, val in enumerate(columns):
-                            # Remove characters and dot which precedes column name for key values
-                            result_obj[re.sub(r'.*[.]', '', val[0])] = str(row[index]).strip()
-                        result_rows.append(result_obj)
-                except Exception, e:
-                    return e
+                        print "done executing query"
 
-        conn.close()
+                        # Get column names
+                        columns = cur.description
+
+                        # Fetch table results
+                        for row in cur:
+                            result_obj = {}
+                            for index, val in enumerate(columns):
+                                # Remove characters and dot which precedes column name for key values
+                                result_obj[re.sub(r'.*[.]', '', val[0])] = str(row[index]).strip()
+                            result_rows.append(result_obj)
+                    except Exception, e:
+                        return e
+
+            conn.close()
+        else:
+            import jaydebeapi
+            import jpype
+
+            try:
+                if not jpype.isJVMStarted():
+                    current_dir = os.path.dirname(os.path.abspath(__file__))
+                    jar = r'{base}/lib/tdgssconfig.jar:{base}/lib/terajdbc4.jar'.format(base=current_dir)
+                    args='-Djava.class.path=%s' % jar
+
+                    if 'JVM_PATH' in os.environ:
+                        jvm_path = os.environ['JVM_PATH']
+                    else:
+                        jvm_path = jpype.getDefaultJVMPath()
+
+                    jpype.startJVM(jvm_path, args)
+
+                with jaydebeapi.connect('com.teradata.jdbc.TeraDriver','jdbc:teradata://{url}/USER={user},PASSWORD={password}'
+                                        .format(url=self.host, user=self.username, password=self.password)) as conn:
+                    with conn.cursor() as cur:
+                        print "executing query"
+
+                        # Execute query
+                        cur.execute(query_string)
+
+                        print "done executing query"
+
+                        # Get column names
+                        columns = cur.description
+
+                        # Fetch table results
+                        for row in cur:
+                            result_obj = {}
+                            for index, val in enumerate(columns):
+                                # Remove characters and dot which precedes column name for key values
+                                result_obj[re.sub(r'.*[.]', '', val[0])] = str(row[index]).strip()
+                            result_rows.append(result_obj)
+
+                    conn.close()
+            except Exception, e:
+                        return e
+
         return result_rows
